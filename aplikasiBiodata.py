9df211a3a34dabf2dc11708beacc97a003a583e4
siti nurviatika <114561174+nurviatika@users.noreply.github.com>
2022-09-28 21:13:30 -0700

    penambahan biodata file

---
+++ aplikasiBiodata.py
@@ -0,0 +1,15 @@
+# aplikasi biodata create vy siti nurviati
+
+# masukan nama
+print("yuk! lengkapi data diri anda..")
+
+# input data diri
+nama = input("nama: ")
+kelas = input("kelas: ")
+prodi = input("program studi: ")
+
+# print / output data diri
+print("nama anda adalah: ", nama)
+print("kelas anda adalah:", kelas)
+
+
