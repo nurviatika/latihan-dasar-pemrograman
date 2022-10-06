d4d24b735c4957b242caa84d5c951cfe7a7a136a
siti nurviatika <114561174+nurviatika@users.noreply.github.com>
2022-10-05 19:20:32 -0700

    penambahan tugassesi3

---
+++ tugassesi3.py
@@ -0,0 +1,8 @@
+# #Aplikasi biodata sederhana
+
+nama = "siti nurviatika"
+kelas = "TI22A"
+jurusan = "teknik informatika"
+quote = "tetap semangat walaupun berat"
+
+print (nama, kelas, jurusan, quote)
